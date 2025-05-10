1c58d20feb909d9243d4db996789c0147b45a7b9
Tomoko Uchida <tomoko.uchida.1111@gmail.com>
2025-05-10 21:58:18 +0900

    exclude colbert model from tests

--- packages/embcli-jina/tests/embcli_jina/test_jina.py
+++ packages/embcli-jina/tests/embcli_jina/test_jina.py
@@ -42,7 +42,7 @@ def test_embed_one_batch_yields_embeddings(jina_models):
 
 
 @skip_if_no_api_key
-def test_embed_batch_with_options(jina_models, mocker):
+def test_embed_batch_with_options(jina_models):
     input_data = ["hello", "world"]
     for model in jina_models:
         if model.model_id == "jina-embeddings-v3":
@@ -55,13 +55,3 @@ def test_embed_batch_with_options(jina_models, mocker):
                 assert isinstance(emb, list)
                 assert all(isinstance(x, float) for x in emb)
                 assert len(emb) == 512
-        elif model.model_id == "jina-colbert-v2":
-            mocker.patch("embcli_jina.jina.COLBERT_TIMEOUT_SEC", 30)
-            options = {"input_type": "query", "dimensions": "64"}
-
-            embeddings = list(model.embed_batch(input_data, None, **options))
-
-            for emb in embeddings:
-                assert isinstance(emb, list)
-                assert all(isinstance(x, float) for x in emb)
-                assert len(emb) == 64
